b9853489b1f48a0bfd77d0bbeccfaadbb7cba7a0
Gaurav <allmanpride@gmail.com>
2021-01-24 16:16:10 +0545

    initial add

---
+++ deepl.py
@@ -0,0 +1,113 @@
+import os
+import re
+import time
+import asyncio
+import json
+
+import selenium
+from selenium import webdriver
+from dotenv import load_dotenv
+
+MAX_DELAY_SEC = 120
+MIN_DELAY_SEC = 5
+
+web = None
+replacements = './replacements.json'
+
+load_dotenv()
+
+def close_web():
+    global web
+    if web:
+        web.close()
+        web = None
+
+async def init_web():
+    global web
+    opt = webdriver.FirefoxOptions()
+    opt.set_headless()
+    prof = webdriver.FirefoxProfile()
+    prof.set_preference("dom.webdriver.enabled", False)
+    prof.set_preference('useAutomationExtension', False)
+    prof.update_preferences()
+
+    print(f'Opening web browser.')
+    web = webdriver.Firefox(firefox_profile=prof,
+                            firefox_options=opt,
+                            desired_capabilities=webdriver.DesiredCapabilities.FIREFOX)
+
+    print('Loading deepl website.')
+    web.get('https://www.deepl.com/translator')
+    await asyncio.sleep(10)
+    try:
+        cookieBtn = web.find_element_by_class_name('dl_cookieBanner--buttonClose')
+        cookieBtn.click()
+    except selenium.common.exceptions.NoSuchElementException:
+        pass
+    return web
+
+async def login():
+    # not used for now.
+    global web
+    print('Logging into the deepl website.')
+    login = web.find_element_by_class_name('dl_header_menu_v2__login_button')
+    login.click()
+    web.find_element_by_id("login_email").send_keys(os.getenv('DEEPL_EMAIL'))
+    web.find_element_by_id("login_password").send_keys(os.getenv('DEEPL_PASS'))
+    login = web.find_element_by_class_name('dl_menu__login__form__submit').click()
+    await asyncio.sleep(5)            # check logged in later
+    return
+
+async def process_text(text):
+    inputarea = web.find_element_by_class_name('lmt__source_textarea')
+    outputarea = web.find_element_by_id('target-dummydiv')
+    
+    inputarea.clear()
+    inputarea.send_keys(text)
+    await asyncio.sleep(MIN_DELAY_SEC)
+    for i in range(MAX_DELAY_SEC-MIN_DELAY_SEC):
+        await asyncio.sleep(1)
+        translated = outputarea.get_attribute('innerHTML')
+        if translated.count('[...]') > 5:
+            continue
+        if translated and outputarea.is_enabled() and inputarea.is_enabled():
+            break
+    return translated
+
+def replace_words(text):
+    if replacements=='' or not os.path.isfile(replacements):
+        rep = dict()
+    else:
+        with open(replacements,'r') as r:
+            rep = json.load(r)
+
+    text = re.sub(r'\n\n+','\n\n',text)
+    for k,v in rep.items():
+        text = text.replace(k,v)
+    return text
+
+
+async def translate(input_file, output_file, paid=False):
+    with open(input_file, 'r') as r:
+        filecontent = replace_words(r.read())
+    if paid:
+        LIMIT = 5000
+    else:
+        LIMIT = 3800
+    lines = filecontent.splitlines()
+    content = ''
+    tl_doc = ''
+    print(f'TRANSLATION: {len(lines)} lines file.')
+    for i,line in enumerate(lines, start=1):
+        if len(content)>LIMIT:
+            tl_doc += await process_text(content) + '\n'
+            print(f'{i} lines completed...')
+            content = ''
+        else:
+            content += line + '\n'
+    
+    tl_doc += await process_text(content)
+    tl_doc = re.sub(r'\n\n+','\n\n',tl_doc)
+    with open(output_file, 'w') as w:
+        w.write(tl_doc)
+    print(f'Written to {output_file}')
---
+++ ncode.py
@@ -0,0 +1,105 @@
+import os
+import re
+import discord
+import scrapper
+import utilities
+
+from dotenv import load_dotenv
+
+bot_pattern = re.compile(r"b! +([a-zA-Z0-9-]+) ?(.*)")
+ncode_pattern = re.compile(r'(https?://)?(ncode.syosetu.com/?)?([a-z0-9]+)/([0-9]+)/?')
+chapter_pattern = re.compile(r'([a-z0-9]+) ?([0-9]+)')
+
+novels = {'rezero':'n2267be'}
+
+if not os.path.isdir('./data'):
+    os.mkdir('data')
+if not os.path.isdir('./rezero'):
+    os.mkdir('rezero')
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord.')
+    for g in client.guilds:
+        print(f'Connected to: {g.name} :id={g.id}')
+    
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    m = ncode_pattern.match(message.content.lower())
+    if m:
+        await utilities.from_ncode(m.group(3), m.group(4), message.channel)
+    m = bot_pattern.match(message.content.lower())
+    if not m:
+        return
+    cmd = m.group(1)
+    args = m.group(2)
+    if cmd == 'help':
+        await utilities.help_message(message.channel)
+    elif cmd == 'ncode':
+        link = ncode_pattern.match(args)
+        if not link:
+            ch = chapter_pattern.match(args)
+            if not ch:
+                await message.channel.send("Send ncode link to get text.")
+                return
+            novel, chapter = parse_novel(ch.group(1), ch.group(2))
+            await utilities.from_ncode(novel, chapter, message.channel)
+        else:
+            await utilities.from_ncode(link.group(3), link.group(4), message.channel)
+    elif cmd == 'deepl':
+        if args:
+            link = ncode_pattern.match(args)
+            if not link:
+                ch = chapter_pattern.match(args)
+                if not ch:
+                    await message.channel.send("Send ncode link to get text.")
+                    return
+                novel, chapter = utilities.parse_novel(ch.group(1), ch.group(2))
+                await utilities.mtl_ncode(novel, chapter, message.channel)
+            else:
+                await utilities.mtl_ncode(link.group(3), link.group(4), message.channel)
+        elif cmd == 'arc7jp':
+            chap = int(args) + 502
+            await utilities.from_ncode(novels['rezero'], chap,
+                             message.channel, filename=f'./rezero/arc7-ch{args}-jp.txt')
+        else:
+            files = os.listdir('./data')
+            chapters = filter(lambda l: re.match(r'[a-z0-9]+_[0-9]+-en.txt',l), files)
+            chaps = '\n'.join(chapters)
+            await message.channel.send(f'Available files: \n{chaps}')
+    elif cmd == 'arc7en':
+        if args:
+            if args.isnumeric():
+                filename = f'./rezero/arc7-ch{args}-en.txt'
+            else:
+                filename = args
+            if os.path.isfile(filename):
+                await utilities.send_file(message.channel, filename)
+            else:
+                chap = int(args) + 502
+                await utilities.mtl_ncode(novels['rezero'], chap,
+                         message.channel, outfile=filename)
+        else:
+            files = os.listdir('./rezero')
+            chapters = filter(lambda l: re.match(r'arc7-ch[0-9]+-en.txt',l), files)
+            chaps = '\n'.join(chapters)
+            await message.channel.send(f'Available files: \n{chaps}')
+
+
+# @client.event
+# async def on_error(*args):
+#     print('Error')
+#     print(args)
+    # await ctx.send(f'Some error occured, contant thevoidzero.')
+
+
+if __name__ == '__main__':
+    client.run(token)
