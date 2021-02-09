78fe6ce2980160e8d2297dfe6ef5cae72d694976
Gaurav <allmanpride@gmail.com>
2021-02-09 14:34:53 +0545

    initial add

---
+++ bot.py
@@ -0,0 +1,48 @@
+import os
+import re
+import discord
+import commands
+
+from dotenv import load_dotenv
+
+bot_pattern = re.compile(r"b! +([a-zA-Z0-9-]+) ?(.*)")
+
+if not os.path.isdir('./data'):
+    os.mkdir('data')
+if not os.path.isdir('./tables'):
+    os.mkdir('tables')
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord.')
+    for g in client.guilds:
+        print(f'Connected to: {g.name} :id={g.id}')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    m = bot_pattern.match(message.content.lower())
+    if not m:
+        return
+    cmd = m.group(1)
+    args = m.group(2)
+    print(f'Command: {cmd} Args: {args}')
+    try:
+        cmd_func = getattr(commands, f'cmd_{cmd.lower()}')
+        await cmd_func(message, args)
+    except AttributeError as e:
+        await message.channel.send('Command not recognized, please use' +
+                                   'help command to get the list.')
+        raise e
+
+
+if __name__ == '__main__':
+    client.run(token)
